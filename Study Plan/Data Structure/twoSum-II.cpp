2ea4f51e5be73ba03d8f21412537bd972f119ddf
Bhavin Patil <59190105+bhavinpatil@users.noreply.github.com>
2022-08-25 09:09:06 +0530

    167. Two Sum II - Input Array Is Sorted

---
+++ Study Plan/Data Structure/twoSum-II.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l = 0;
+        int h = numbers.size()-1;
+        while(l<h)
+        {
+            if(numbers[l]+numbers[h]==target) return {l+1, h+1};
+            else if(numbers[l]+numbers[h]>target) h--;
+            else l++;
+        }
+        return {};
+    }
+};
