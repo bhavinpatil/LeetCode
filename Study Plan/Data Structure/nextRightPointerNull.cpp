096e45518d054e5320dc6e8b0178c17456320d49
Bhavin Patil <59190105+bhavinpatil@users.noreply.github.com>
2022-08-30 23:46:01 +0530

    Update nextRightPointerNull.cpp

--- Study Plan/Data Structure/nextRightPointerNull.cpp
+++ Study Plan/Data Structure/nextRightPointerNull.cpp
@@ -39,3 +39,25 @@ public:
         return root;
     }
 };
+
+
+/*=======================OR================================*/
+
+void connect(TreeLinkNode *root) {
+    if(!root)
+        return;
+    while(root -> left)
+    {
+        TreeLinkNode *p = root;
+        while(p)
+        {
+            p -> left -> next = p -> right;
+            if(p -> next)
+                p -> right -> next = p -> next -> left;
+            p = p -> next;
+        }
+        root = root -> left;
+    }
+}
+
+
